240055783001da1c5d42624cb83bf4502caf75bb
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-05-26 13:31:24 +0600

    Add tables and initial data for conservation database

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,34 @@
 -- Active: 1747545730198@@localhost@5432@conservation_db
+
+-- CREATE DATABASE conservation_db;
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region TEXT NOT NULL
+);
+
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name TEXT NOT NULL,
+    scientific_name TEXT NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status TEXT   -- Endangered  || Vulnerable
+);
+
+
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT NOT NULL REFERENCES rangers(ranger_id),
+    species_id INT NOT NULL REFERENCES species(species_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location TEXT NOT NULL,
+    notes TEXT
+);
+
+INSERT INTO rangers (name, region) VALUES
+('Derek Fox','Coastal Plains');
+
+
